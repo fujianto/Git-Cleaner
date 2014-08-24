a2590d85f626e715d11eebcb2d3c98d56df20136
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2014-08-24 23:50:04 +0700

    Initial commit

---
+++ GitCleaner/MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace GitCleaner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFolder = new MetroFramework.Controls.MetroButton();
+            this.lblInfo = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Location = new System.Drawing.Point(23, 81);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(121, 46);
+            this.btnOpenFolder.TabIndex = 0;
+            this.btnOpenFolder.Text = "Open";
+            this.btnOpenFolder.UseSelectable = true;
+            this.btnOpenFolder.Click += new System.EventHandler(this.btnOpenFolder_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(24, 153);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(45, 19);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "lblInfo";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 256);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Name = "MainForm";
+            this.Text = "Git Cleaner 0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton btnOpenFolder;
+        private MetroFramework.Controls.MetroLabel lblInfo;
+
+
+
+    }
+}
+
---
+++ GitCleaner/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using xDialog;
+using System.IO;
+
+
+namespace GitCleaner
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            lblInfo.Text = "";
+        }
+
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            string folderPath = "";
+            FolderBrowserDialog fbdGit = new FolderBrowserDialog();
+            DialogResult dr = fbdGit.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+                try
+                {
+                    folderPath = fbdGit.SelectedPath;
+                    string[] fileTarget = Directory.GetFiles(@folderPath, ".git*", SearchOption.AllDirectories);
+                    var di = new DirectoryInfo(@folderPath + "\\.git");
+
+                    //Delete all files started with .git
+                    for (int i = 0; i < fileTarget.Length; i++)
+                    {
+                        File.Delete(fileTarget[i]);
+                        Console.WriteLine(fileTarget[i] + " Deleted");
+                    }
+
+                    Console.WriteLine("All *.git* files deleted");
+
+                    //Delete .git directory
+                    foreach (var file in di.GetFiles("*", SearchOption.AllDirectories))
+                    {
+                        file.Attributes &= ~FileAttributes.ReadOnly;
+                    }
+
+                    Console.WriteLine("All hidden attribute stripped");
+
+                    System.IO.Directory.Delete(@folderPath + "\\.git", true);
+                    MetroFramework.MetroMessageBox.Show(this, "All .git Removed", "Successs", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MsgBox.Show(ex.Message, "Exception", MsgBox.Buttons.OK, MsgBox.Icon.Error);
+                }
+ 
+            }
+        }
+
+ 
+    }
+}
